1c9328f3265ffe21e4b175afe068fe913a013805
Đỗ Lê Duy <doleduy1999@gmail.com>
2023-02-19 00:22:20 +0700

    Update userSlice.ts

--- store/userSlice.ts
+++ store/userSlice.ts
@@ -1,36 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { AppState } from "./store";
-import { HYDRATE } from "next-redux-wrapper";
+import { createSlice } from '@reduxjs/toolkit';
+import { AppState } from './store';
+import { HYDRATE } from 'next-redux-wrapper';
 
 interface initialState {
-  user: IUser | null
+  user: IUser | null;
 }
 
 const initialState: initialState = {
-  user: null
+  user: null,
+};
+
+const extraReducers: any = {
+  [HYDRATE]: (state: any, action: any) => {
+    return {
+      ...state,
+      ...action.payload,
+    };
+  },
 }
 
 export const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState,
   reducers: {
     setUser(state, action) {
       state.user = action.payload;
     },
-
-    extraReducers: {
-      [HYDRATE]: (state, action) => {
-        return {
-          ...state,
-          ...action.payload,
-        }
-      },
-    },
+    extraReducers
   },
 });
 
 export const { setUser } = userSlice.actions;
 
-export const getUser = (state: AppState) => state.user
+export const getUser = (state: AppState) => state.user;
 
 export default userSlice.reducer;
