018f07c3d2f386a5e90a726b7976f70707208e3a
Nguyễn Bá Khoa <khoa.nguyenba@outlook.com>
2023-02-19 00:44:41 +0700

    Format code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,35 @@
-import Layout from '@/components/layout';
-import '@/styles/globals.scss';
-import type { AppProps } from 'next/app';
-import { wrapper } from '@/store/store';
-import { Provider } from 'react-redux';
+import Layout from "@/components/layout";
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
+import { wrapper } from "@/store/store";
+import { Provider } from "react-redux";
+import { useEffect, useState } from "react";
+import { setUser } from "@/store/userSlice";
+import { getUserByToken } from "./api/api";
 
 function App({ Component, ...rest }: AppProps) {
-  const {store, props} = wrapper.useWrappedStore(rest);
+  const [loading, setLoading] = useState(true);
+  const { store, props } = wrapper.useWrappedStore(rest);
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      getUserByToken(token).then((res) => {
+        setLoading(false);
+        store.dispatch(setUser(res.data));
+      });
+    }
+  }, []);
+
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...props.pageProps} />
-      </Layout>
+      {loading ? null : (
+        <Layout>
+          <Component {...props.pageProps} />
+        </Layout>
+      )}
     </Provider>
   );
 }
 
-export default App
+export default App;
